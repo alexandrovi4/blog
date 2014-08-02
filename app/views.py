33f4befb2e9b7e1d1b083be5e4c81c3429a830d5
alexandrovi4 <richik.kirill@yandex.ru>
2014-08-03 01:32:55 +0400

    added template for html pages

--- app/views.py
+++ app/views.py
@@ -1,7 +1,24 @@
 __author__ = 'kirill'
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "this is test page"
+    user = {'nick': 'Xy9se'} # fake
+    posts = [
+        {
+            'author': {'nickname':'John'},
+            'body':'beautiful day in Portland'
+        },
+        {
+            'author':{'nickname':'Tom'},
+            'body':'Hi. How do you fly?'
+        }
+    ]
+
+    return render_template("index.html",
+                           title="Home",
+                           user=user,
+                           posts=posts
+                           )
