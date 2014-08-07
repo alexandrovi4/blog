23a1eeb345ce775fb7f79312fc24dc6e68111f3a
alexandrovi4 <richik.kirill@yandex.ru>
2014-08-08 02:22:17 +0400

    added profile page

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 __author__ = 'kirill'
 
 from app import db
+from hashlib import md5
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -23,6 +24,9 @@ class User(db.Model):
 
     def get_id(self):
         return unicode(self.id)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
