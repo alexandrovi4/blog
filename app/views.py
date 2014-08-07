b9fd8938be27587bd1636ccd4686bdddf260b5e7
alexandrovi4 <richik.kirill@yandex.ru>
2014-08-08 00:20:33 +0400

    added function for next action after user loginned

--- app/views.py
+++ app/views.py
@@ -39,6 +39,26 @@ def login():
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
 
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember = False
+    if 'remember' in session:
+        remember = session['remember']
+        session.pop('remember', None)
+    login_user(user, remember = remember)
+    return redirect(request.args.get('next') or url_for('index'))
+
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
